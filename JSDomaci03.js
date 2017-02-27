acc5d354bcf85a4b5a48dd9455bef342b4074c31
Stefan <Stefanb87@users.noreply.github.com>
2017-02-27 12:02:24 +0100

    Update JSDomaci03.js

--- JSDomaci03.js
+++ JSDomaci03.js
@@ -1,40 +1,51 @@
-var cene = [5, 145, 69, 56, 13, 456, 78, 4, 30];
-var cenaPorez = [];
+var niz = [5, 145, 69, 56, 13, 456, 78, 4, 30];
+porez(niz);
 
-for(var i=0; i<cene.length; i++) {
-	if (cene[i]<50) {
-		cenaPorez.push((cene[i]*1.18).toFixed(2));
-	} else {
-		cenaPorez.push((cene[i]*1.08).toFixed(2));
-	}
-}
-console.log(cenaPorez);
+function porez(cene) {
+	var cenaPorez = [];
 
-var rec = "anavolimilovana";
-rec.toLowerCase();
-var brojSlova = rec.length;
-var novaRec = [];
-
-if (brojSlova%2==0) {
-	for(var i=0; i<rec.length/2; i++) {
-		if (rec[i]===rec[(brojSlova-1)-i]) {
-			novaRec.push(rec[i]);
-			if (novaRec.length==rec.length/2) {
-				console.log('Rec ' + rec + ' je palindrom.');
-			}
+	for(var i=0; i<cene.length; i++) {
+		if (cene[i]<50) {
+			cenaPorez.push((cene[i]*1.18).toFixed(2));
 		} else {
-			console.log('Rec ' + rec + ' nije palindrom.');
+			cenaPorez.push((cene[i]*1.08).toFixed(2));
 		}
 	}
-} else {
-	for(var i=0; i<(rec.length-1)/2; i++) {
-		if (rec[i]===rec[(brojSlova-1)-i]) {
-			novaRec.push(rec[i]);
-			if (novaRec.length==(rec.length-1)/2) {
-					console.log('Rec ' + rec + ' je palindrom.');
+	return console.log(cenaPorez);
+}
+
+
+
+var string = "ananaf";
+palindrom(string);
+
+function palindrom(rec) {
+	
+	rec.toLowerCase();
+	var brojSlova = rec.length;
+	var novaRec = [];
+
+	if (brojSlova%2==0) {
+		for(var i=0; i<rec.length/2; i++) {
+			if (rec[i]===rec[(brojSlova-1)-i]) {
+				novaRec.push(rec[i]);
+				if (novaRec.length==rec.length/2) {
+					return console.log('Rec ' + rec + ' je palindrom.');
+				}
+			} else {
+				return console.log('Rec ' + rec + ' nije palindrom.');
 			}
-		} else {
-		console.log('Rec ' + rec + ' nije palindrom.');
+		}
+	} else {
+		for(var i=0; i<(rec.length-1)/2; i++) {
+			if (rec[i]===rec[(brojSlova-1)-i]) {
+				novaRec.push(rec[i]);
+				if (novaRec.length==(rec.length-1)/2) {
+					return console.log('Rec ' + rec + ' je palindrom.');
+				}
+			} else {
+				return console.log('Rec ' + rec + ' nije palindrom.');
+			}
 		}
 	}
 }
